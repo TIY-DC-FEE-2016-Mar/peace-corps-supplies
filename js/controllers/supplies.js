5bf73c8aa1f3020fb46ac347379fdcd76340108c
James Dabbs <jamesdabbs@gmail.com>
2016-04-19 09:54:59 -0400

    Hardcode list of items

--- js/controllers/supplies.js
+++ js/controllers/supplies.js
@@ -7,5 +7,11 @@
 
   function SuppliesController() {
     this.user = "James";
+
+    this.items = [
+      { name: "advil", inCountry: true, globally: true },
+      { name: "bandages", inCountry: false, globally: true },
+      { name: "mosquitos", inCountry: true, globally: true }
+    ]
   }
 })();
