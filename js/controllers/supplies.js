9bc73ce6b095792228d581440d85ac8f73fd3666
James Dabbs <jamesdabbs@gmail.com>
2016-04-20 09:43:55 -0400

    Fetch data from factory

--- js/controllers/supplies.js
+++ js/controllers/supplies.js
@@ -5,12 +5,12 @@
     .module("medlink")
     .controller("SuppliesController", SuppliesController);
 
-  function SuppliesController() {
-    this.items = [
-      { name: "bandages", inCountry: false, globally: true, description: "A bandage is a piece of material used either to support a medical device such as a dressing or splint, or on its own to provide support to or to restrict the movement of a part of the body." },
-      { name: "advil", inCountry: true, globally: true, description: "Ibuprofen, from isobutylphenylpropanoic acid, is a nonsteroidal anti-inflammatory drug (NSAID) used for treating pain, fever, and inflammation." },
-      { name: "mosquitos", inCountry: true, globally: true, description: "Mosquitoes are small, midge-like flies which comprise the family Culicidae. Females of most species are ectoparasites, whose tube-like mouthparts (called a proboscis) pierce the hosts' skin to consume blood." }
-    ];
+  SuppliesController.$inject = ["supplyStore"];
+
+  function SuppliesController(items) {
+    console.log('items', items);
+
+    this.items = items;
 
     this.capitalize = function capitalize(word) {
       if (!word) { return ""; }
